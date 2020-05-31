20060919333a4579a68761163cf7a9790aa1fbaa
Phạm Hữu Thọ <36005079+Phamthous@users.noreply.github.com>
2020-05-31 15:39:32 +0700

    Update project_final_examination.m

--- source code/project_final_examination.m
+++ source code/project_final_examination.m
@@ -1,87 +1,76 @@
-<<<<<<< HEAD
-clc;
-
-clear;
-
-
 Fs = 22050;
+Ts = 1/Fs;
 
-delta = 1/Fs;
+FC5 = 261.63;
+FD5 = 293.66;
+FE5 = 329.63;
+FF5 = 349.23;
+FG5 = 392.00;
+FA5 = 440.00;
+FB5 = 493.88;
 
-FC5 = 523.25;
+d1 = 1;
+d2 = 0.5; 
+d3 = 1; 
 
-FD5 = 587.33;
+dt1=Ts:Ts:d1;
+dt2=Ts:Ts:d2;
+dt3=Ts:Ts:d3;
 
-FE5 = 659.25;
+PC5_dt2 = zeros(1,Fs*d2)
+PA5_dt2 = zeros(1,Fs*d2);
+PE5_dt2 = zeros(1,Fs*d2);
+PF5_dt2 = zeros(1,Fs*d2);
+PB5_dt2 = zeros(1,Fs*d2);
+PD5_dt2 = zeros(1,Fs*d2);
+PG5_dt2 = zeros(1,Fs*d2);
 
-FF5 = 698.46;
+PC5_dt1 = zeros(1,Fs*d1)
+PA5_dt1 = zeros(1,Fs*d1);
+PE5_dt1 = zeros(1,Fs*d1);
+PF5_dt1 = zeros(1,Fs*d1);
+PB5_dt1 = zeros(1,Fs*d1);
+PD5_dt1 = zeros(1,Fs*d1);
+PG5_dt1 = zeros(1,Fs*d1);
 
-FG5 = 783.99;
+PC5_dt3 = zeros(1,Fs*d3)
+PA5_dt3 = zeros(1,Fs*d3);
+PE5_dt3 = zeros(1,Fs*d3);
+PF5_dt3 = zeros(1,Fs*d3);
+PB5_dt3 = zeros(1,Fs*d3);
+PD5_dt3 = zeros(1,Fs*d3);
+PG5_dt3 = zeros(1,Fs*d3);
 
-FA5 = 880;
-
-FB5 = 987.77;
-
-
-d1 = 2;
-
-d05 = 1;
-
-d15 = 3;
-
-t1 = delta:delta:d1;
-
-t05 = delta:delta:d05;
-
-t15 = delta:delta:d15;
-
-
-PC4_1 = zeros(1,Fs*d1);
-
-PD5_1 = zeros(1,Fs*d1);
-
-PD5_05 = zeros(1,Fs*d05);
-
-PA5_1 = zeros(1,Fs*d1);
-
-PA5_05 = zeros(1,Fs*d05);
-
-PE5_05 = zeros(1,Fs*d05);
-
-PF5_05 = zeros(1,Fs*d05);
-
-PD5_15 = zeros(1,Fs*d15);
-
-
-p = [1 0.1 0.33 0.06 0.05 0.045 0 0.02 0.005 0.005 0 0.005 0.01];
+Amplitude = [1 0.1 0.33 0.06 0.05 0.045 0 0.02 0.005 0.005 0 0.005 0.01];
 
 for n=1:length(p);
 
- %PC4_1 = PC4_1 + p(n)*exp((-1)*t1*4).*cos(2*pi*n*FC5*t1); % sythesize waveform
- PD5_1 = PD5_1 + p(n)*exp((-1)*t1*3).*cos(2*pi*n*FD5*t1);
- PD5_05 = PD5_05 + p(n)*exp((-1)*t05*3).*cos(2*pi*n*FD5*t05);
- PD5_15 = PD5_15 + p(n)*exp((-1)*t15*3).*cos(2*pi*n*FD5*t15);
- PA5_1 = PA5_1 + p(n)*exp((-1)*t1*3).*cos(2*pi*n*FA5*t1);
- PA5_05 = PA5_05 + p(n)*exp((-1)*t05*3).*cos(2*pi*n*FA5*t05); 
- PE5_05 = PE5_05 + p(n)*exp((-1)*t05*3).*cos(2*pi*n*FE5*t05); 
- PF5_05 = PF5_05 + p(n)*exp((-1)*t05*3).*cos(2*pi*n*FF5*t05);
+ PC5_dt1 = PC5_dt1 + Amplitude(n)*exp((-1)*dt1*3).*cos(2*pi*n*FC5*dt1);
+ PD5_dt1 = PD5_dt1 + Amplitude(n)*exp((-1)*dt1*3).*cos(2*pi*n*FD5*dt1);
+ PG5_dt1 = PG5_dt1 + Amplitude(n)*exp((-1)*dt1*3).*cos(2*pi*n*FG5*dt1);
+ PA5_dt1 = PA5_dt1 + Amplitude(n)*exp((-1)*dt1*3).*cos(2*pi*n*FA5*dt1); 
+ PE5_dt1 = PE5_dt1 + Amplitude(n)*exp((-1)*dt1*3).*cos(2*pi*n*FE5*dt1); 
+ PF5_dt1 = PF5_dt1 + Amplitude(n)*exp((-1)*dt1*3).*cos(2*pi*n*FF5*dt1);
+ PB5_dt1 = PB5_dt1 + Amplitude(n)*exp((-1)*dt1*3).*cos(2*pi*n*FB5*dt1);
+ 
+ PC5_dt2 = PC5_dt2 + Amplitude(n)*exp((-1)*dt2*3).*cos(2*pi*n*FC5*dt2);
+ PD5_dt2 = PD5_dt2 + Amplitude(n)*exp((-1)*dt2*3).*cos(2*pi*n*FD5*dt2);
+ PG5_dt2 = PG5_dt2 + Amplitude(n)*exp((-1)*dt2*3).*cos(2*pi*n*FG5*dt2);
+ PA5_dt2 = PA5_dt2 + Amplitude(n)*exp((-1)*dt2*3).*cos(2*pi*n*FA5*dt2); 
+ PE5_dt2 = PE5_dt2 + Amplitude(n)*exp((-1)*dt2*3).*cos(2*pi*n*FE5*dt2); 
+ PF5_dt2 = PF5_dt2 + Amplitude(n)*exp((-1)*dt2*3).*cos(2*pi*n*FF5*dt2);
+ PB5_dt2 = PB5_dt2 + Amplitude(n)*exp((-1)*dt2*3).*cos(2*pi*n*FB5*dt2);
+ 
+ PC5_dt3 = PC5_dt3 + Amplitude(n)*exp((-1)*dt3*3).*cos(2*pi*n*FC5*dt3);
+ PD5_dt3 = PD5_dt3 + Amplitude(n)*exp((-1)*dt3*3).*cos(2*pi*n*FD5*dt3);
+ PG5_dt3 = PG5_dt3 + Amplitude(n)*exp((-1)*dt3*3).*cos(2*pi*n*FG5*dt3);
+ PA5_dt3 = PA5_dt3 + Amplitude(n)*exp((-1)*dt3*3).*cos(2*pi*n*FA5*dt3); 
+ PE5_dt3 = PE5_dt3 + Amplitude(n)*exp((-1)*dt3*3).*cos(2*pi*n*FE5*dt3); 
+ PF5_dt3 = PF5_dt3 + Amplitude(n)*exp((-1)*dt3*3).*cos(2*pi*n*FF5*dt3);
+ PB5_dt3 = PB5_dt3 + Amplitude(n)*exp((-1)*dt3*3).*cos(2*pi*n*FB5*dt3);
 end
 
-y = [PD5_1 PD5_05 PA5_1 PA5_05 PE5_05 PF5_05 PE5_05 PD5_15];
-plot(y)
+y = [PC5_dt2 PC5_dt2 PD5_dt2 PC5_dt2 PA5_dt2 PG5_dt1 PC5_dt2 PC5_dt2 PD5_dt2 PC5_dt2 PE5_dt2 PA5_dt1];
+plot(y);
 player=audioplayer(y,Fs);
 play(player);
-=======
-Fs=44100;
-m=2*Fs;
-c=zeros(1,m);
-n=int32(Fs/440.0*2);
-b=rand(1,n)-0.5
-for i=1:m
-    b(mod(i-1,n)+1)=0.5*(b(mod(i-1,n)+1)+ b(mod(i,n)+1));
-    c(i)= b(mod(i-1,n)+1);
-end
-plot(c);
-player=audioplayer(c,Fs);
-play(player);
->>>>>>> 4ffe79bfea332342e7067548c40e386b09a9c130
